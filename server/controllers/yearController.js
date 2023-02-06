e219f2650939d5e1166d61c9a1275685e24ca725
cosisaxis <kishanelewis2@gmail.com>
2023-02-06 10:52:51 -0500

    data fetching

--- server/controllers/yearController.js
+++ server/controllers/yearController.js
@@ -2,22 +2,10 @@
  const Years = require('../models/year');
 
 exports.listYears = async(req, res) => {
- 
-}
-
-async function insertDay(){
     try{
-        await Years.insertMany([
-
-            {
-                "day": 2022-08-08,
-                "event": "yeah he did this hahaa",
-                "category": ["Basketball"]
-            }
-
-        ])
-
-    }catch (error) {
-      console.log(error);
+        const year = await Years.find({});
+        res.json(year);
+    } catch(err){
+        res.status(400).json({message: err});
     }
 }
