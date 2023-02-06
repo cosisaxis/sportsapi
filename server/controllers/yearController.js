fe0c55d64f13ac6291185ea9dffbbd6395134ad1
cosisaxis <kishanelewis2@gmail.com>
2023-02-06 11:30:39 -0500

    api-call-limit

--- server/controllers/yearController.js
+++ server/controllers/yearController.js
@@ -2,10 +2,14 @@
  const Years = require('../models/year');
 
 exports.listYears = async(req, res) => {
+
+    let { limit } = req.query;
+    const limitRecord = parseInt(limit);
+
     try{
-        const year = await Years.find({});
+        const year = await Years.find({}).limit(limitRecord);
         res.json(year);
     } catch(err){
         res.status(400).json({message: err});
-    }
+    } 
 }
